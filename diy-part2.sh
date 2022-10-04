8209089364391b34d2dbf027351ff10a7ec33426
duthfox <g1226cl@gmail.com>
2022-10-04 16:16:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,9 +12,9 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-# sed -i 's/+IPV6:libip6tc//g' package/network/config/firewall/Makefile
-# sed -i 's/+IPV6:kmod-nf-conntrack6//g' package/network/config/firewall/Makefile
-# sed -i 's/+IPV6:libip6tc//g' package/network/utils/iptables/Makefile
+sed -i 's/+IPV6:libip6tc//g' package/network/config/firewall/Makefile
+sed -i 's/+IPV6:kmod-nf-conntrack6//g' package/network/config/firewall/Makefile
+sed -i 's/+IPV6:libip6tc//g' package/network/utils/iptables/Makefile
 #cp target/linux/ath79/dts/ar9344_netgear_wndr.dtsi target/linux/ath79/dts/ar9344_netgear_wndr_128m.dtsi
 #sed -i 's/ar9344_netgear_wndr.dtsi/ar9344_netgear_wndr_128m.dtsi/g' target/linux/ath79/dts/ar9344_netgear_wndr_128m.dtsi
 #sed -i 's/ubiconcat0: partition@ac0000/partition@ac0000/g;s/ubiconcat0/ubi/g;s/0x1500000/0x7500000/g' target/linux/ath79/dts/ar9344_netgear_wndr_128m.dtsi
